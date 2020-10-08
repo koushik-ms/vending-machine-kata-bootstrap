437ff90db844216e7a73423b7ff5bac2ed68df1b
Koushik MS <koushik.ms@gmail.com>
2020-10-08 12:04:34 +0000

    Add failing test, improve repl.it integration.

---
+++ vending_machine_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+class VendinMachineTestCase(unittest.TestCase):
+  # Any function inside this class whose name 
+  # starts with 'test...' is a test
+  def test_the_answer(self):
+    self.assertEqual(42, "fixme")
+
+if __name__ == '__main__':
+  unittest.main()
